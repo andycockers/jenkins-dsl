86b3b6cd28c5a7bb17481d3d4e66aa914cf3dd4a
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-02 11:53:27 +0000

    changed packer builder

--- vars/packer_test.groovy
+++ vars/packer_test.groovy
@@ -2,7 +2,7 @@ def call () {
     script {
         checkout([$class: 'GitSCM', branches: [[name: "*/master"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-3', url: "git@github.com:andycockers/gcp-automation.git"]]]) 
         //sh "cat serviceaccount.json"
-        sh "cd packer/install-microk8s && packer build install-microk8s.json"
+        sh "cd packer/install-install-jenkins-deps && packer build install-jenkins-deps.json"
         
     }       
  }
