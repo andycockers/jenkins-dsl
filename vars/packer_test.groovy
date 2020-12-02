b888a4041eaee8de016b05fc96bc02f67930e898
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-02 14:11:13 +0000

    added vault pass

--- vars/packer_test.groovy
+++ vars/packer_test.groovy
@@ -2,6 +2,7 @@ def call () {
     script {
         checkout([$class: 'GitSCM', branches: [[name: "*/master"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-3', url: "git@github.com:andycockers/gcp-automation.git"]]]) 
         //sh "cat serviceaccount.json"
+        sh "echo $VAULT > password"
         sh "cd packer/install-jenkins-deps-hetzner && packer build install-jenkins-deps-hetzner.yml"
         
     }       
