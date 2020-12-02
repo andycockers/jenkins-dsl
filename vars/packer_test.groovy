2c0a6690279cc7b85681ac6bd61640701de0bb1a
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-02 11:56:58 +0000

    arf

--- vars/packer_test.groovy
+++ vars/packer_test.groovy
@@ -2,7 +2,7 @@ def call () {
     script {
         checkout([$class: 'GitSCM', branches: [[name: "*/master"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-3', url: "git@github.com:andycockers/gcp-automation.git"]]]) 
         //sh "cat serviceaccount.json"
-        sh "cd packer/install-jenkins-deps && packer build install-jenkins-deps.json"
+        sh "cd packer/install-jenkins-deps-hetzner && packer build install-jenkins-deps-hetzner.json"
         
     }       
  }
