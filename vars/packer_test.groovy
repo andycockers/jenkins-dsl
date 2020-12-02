438583cac9075c5938e9fe1a7d0633b06f620384
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-02 14:40:33 +0000

    added vault to script

--- vars/packer_test.groovy
+++ vars/packer_test.groovy
@@ -1,9 +1,11 @@
 def call () {
     script {
         checkout([$class: 'GitSCM', branches: [[name: "*/master"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-3', url: "git@github.com:andycockers/gcp-automation.git"]]]) 
-        //sh "cat serviceaccount.json"
+        withCredentials([string(credentialsId: 'ansible_vault', variable: 'VAULT')]) {
         sh "echo $VAULT > password"
         sh "cd packer/install-jenkins-deps-hetzner && packer build install-jenkins-deps-hetzner.yml"
+}
+        
         
     }       
  }
