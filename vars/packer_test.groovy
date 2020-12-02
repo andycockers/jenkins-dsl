96f013932f3200a9c3605ee2ad5a48c23c185b36
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-02 11:41:18 +0000

    changed git

--- vars/packer_test.groovy
+++ vars/packer_test.groovy
@@ -1,8 +1,8 @@
 def call () {
     script {
-        checkout([$class: 'GitSCM', branches: [[name: "*/master"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'appno_bitbucket', url: "git@bitbucket.org:appno/paas-multi-terraform-library.git"]]]) 
+        checkout([$class: 'GitSCM', branches: [[name: "*/master"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-3', url: "git@github.com:andycockers/gcp-automation.git"]]]) 
         //sh "cat serviceaccount.json"
-        sh "cd packer/create-jenkins-gsutil-slave && packer build create-jenkins-gsutil-slave.json"
+        sh "cd packer/install-microk8s && packer build install-microk8s.json"
         
     }       
  }
