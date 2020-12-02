0c4fe7f9fed48cf2a0205ec11152f04b696b06c6
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-02 17:21:14 +0000

    ooaaa

--- vars/packer_test.groovy
+++ vars/packer_test.groovy
@@ -2,7 +2,7 @@ def call () {
     script {
         checkout([$class: 'GitSCM', branches: [[name: "*/master"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-3', url: "git@github.com:andycockers/gcp-automation.git"]]]) 
         withCredentials([string(credentialsId: 'ansible_vault', variable: 'VAULT')]) {
-        sh "cd packer/install-jenkins-deps-hetzner && echo ${VAULT} | base64 -d > password && packer build install-jenkins-deps-hetzner.yml"
+        sh "cd packer/install-jenkins-deps-hetzner && echo $VAULT > password && packer build install-jenkins-deps-hetzner.yml"
 }
         
         
