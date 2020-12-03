a7d536c8ab488a05a3a79bf6f5af73aa7b6548b5
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-03 15:24:29 +0000

    meep

--- vars/create_terratest_image.groovy
+++ vars/create_terratest_image.groovy
@@ -1,6 +1,7 @@
 def call () {
     script {
-        checkout([$class: 'GitSCM', branches: [[name: "*/master"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github', url: "git@github.com:andycockers/gcp-automation.git"]]]) 
+        checkout([$class: 'GitSCM', branches: [[name: "*/master"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github', url: "git@github.com:andycockers/gcp-automation.git"]]])
+        sh "cd /usr/local/bin && wget https://releases.hashicorp.com/packer/1.6.5/packer_1.6.5_linux_amd64.zip && unzip packer_1.6.5_linux_amd64.zip"
         sh "cd packer/create-jenkins-gsutil-slave && packer build create-jenkins-gsutil-slave.json"
 
         
