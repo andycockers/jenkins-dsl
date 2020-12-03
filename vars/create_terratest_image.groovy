55f8c0ccc4d8b23f3b68398b948f8b85b569b762
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-03 14:42:43 +0000

    create terratest image

---
+++ vars/create_terratest_image.groovy
@@ -0,0 +1,10 @@
+def call () {
+    script {
+        checkout([$class: 'GitSCM', branches: [[name: "*/master"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github', url: "git@github.com:andycockers/gcp-automation.git"]]]) 
+        node('swarm')  {
+        sh "cd packer/create-jenkins-gsutil-slave && packer build create-jenkins-gsutil-slave.json"
+}
+        
+        
+    }       
+ }
