4c6df7430de1a8512fd1a41316d0947e5d4cb087
Andy Cockerill <andrew.cockerill@appnovation.com>
2020-12-03 21:48:47 +0000

    oof

--- vars/terratest_service_account.groovy
+++ vars/terratest_service_account.groovy
@@ -1,7 +1,7 @@
 def call () {
     script {
         checkout([$class: 'GitSCM', branches: [[name: "*/master"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github', url: "git@github.com:andycockers/kubernetes.git"]]]) 
-        withCredentials([string(credentialsId: 'jenkins-test-project', variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
+        withCredentials([file(credentialsId: 'jenkins-test-project', variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
         sh "cd terratest/gcp/service-account && go mod init github.com/gruntwork-io/terratest/tree/master/modules/terraform"
         sh "cd terratest/gcp/service-account && go test"
 }
