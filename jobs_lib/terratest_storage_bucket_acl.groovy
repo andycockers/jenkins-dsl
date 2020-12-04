ea8fce63a367d23433542a8d41c0ce5624ce7265
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-04 17:01:46 +0000

    terratest storage bucket acl

---
+++ jobs_lib/terratest_storage_bucket_acl.groovy
@@ -0,0 +1,25 @@
+pipelineJob("terratest/terratest-storage-bucket-acl") {
+	description()
+	keepDependencies(false)
+
+	//parameters {
+	//	stringParam("CLUSTER_LOCATION", "amer", "gcp cluster locationd")
+	//	stringParam("HOST_REGION", "us-west1", "gcp cluster region")
+	//}
+
+	definition {
+		cpsScm {
+			scm {
+				git {
+					remote {
+						url("git@github.com:andycockers/jenkins-dsl.git")
+						credentials("github")
+					}
+					branch("*/main")
+				}
+			}
+			scriptPath("jenkinsfile_lib/terratest_storage_bucket_acl")
+		}
+	}
+	disabled(false)
+}
