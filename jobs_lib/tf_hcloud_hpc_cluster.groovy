8019254b1434172a63070672029ba1bc18a945dd
Andrew Cockerill <andrew.cockerill@appnovation.com>
2021-01-14 13:51:34 +0000

    wowzers

---
+++ jobs_lib/tf_hcloud_hpc_cluster.groovy
@@ -0,0 +1,25 @@
+pipelineJob("terraform/terraform-hcloud-hpc-cluster") {
+	description()
+	keepDependencies(false)
+
+	//parameters {
+	//	stringParam("CLUSTER_LOCATION", "amer", "gcp cluster locationd")
+	//	stringParam("HOST_REGION", "us-west1", "gcp cluster region")
+	//}
+
+	definition {
+		cpsScm {
+			scm {
+				git {
+					remote {
+						url("git@github.com:andycockers/jenkins-dsl.git")
+						credentials("github")
+					}
+					branch("*/main")
+				}
+			}
+			scriptPath("jenkinsfile_lib/terraform_hcloud_hpc_cluster")
+		}
+	}
+	disabled(false)
+}
