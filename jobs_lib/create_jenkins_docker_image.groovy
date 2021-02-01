ca9219f89f3a10c4356065348270dbdc459efc3a
Andy Cockerill <andrew.cockerill@appnovation.com>
2021-02-01 20:40:19 +0000

    new job

---
+++ jobs_lib/create_jenkins_docker_image.groovy
@@ -0,0 +1,25 @@
+pipelineJob("dsl-jobs/create-jenkins-docker-image") {
+	description()
+	keepDependencies(false)
+
+	//parameters {
+	//	stringParam("CLUSTER_LOCATION", "amer", "gcp cluster locationd")
+	//	stringParam("HOST_REGION", "us-west1", "gcp cluster region")
+	//}
+
+	definition {
+		cpsScm {
+			scm {
+				git {
+					remote {
+						url("git@github.com:andycockers/jenkins-dsl.git")
+						credentials("github")
+					}
+					branch("*/main")
+				}
+			}
+			scriptPath("jenkinsfile_lib/create_jenkins_docker_image")
+		}
+	}
+	disabled(false)
+}
