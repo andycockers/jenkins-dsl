58f2e1a7c54607373d20789a046bd6f0d63c85c1
Andy Cockerill <andrew.cockerill@appnovation.com>
2021-07-09 17:44:00 +0100

    destroy

---
+++ jobs_lib/tf_hcloud_hpc_cluster_destroy.groovy
@@ -0,0 +1,25 @@
+pipelineJob("terraform/terraform-hcloud-hpc-cluster-destroy") {
+	description()
+	keepDependencies(false)
+
+	//parameters {
+	//	stringParam("CLUSTER_LOCATION", "amer", "gcp cluster locationd")
+	//	stringParam("HOST_REGION", "us-west1", "gcp cluster region")
+	//}
+
+	definition {
+		cpsScm {
+			scm {
+				git {
+					remote {
+						url("git@github.com:andycockers/jenkins-dsl.git")
+						credentials("github")
+					}
+					branch("*/main")
+				}
+			}
+			scriptPath("jenkinsfile_lib/terraform_hcloud_hpc_cluster_destroy")
+		}
+	}
+	disabled(false)
+}
