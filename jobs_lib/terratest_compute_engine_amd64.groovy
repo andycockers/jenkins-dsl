a73db0c27d7e24c11a57a8f5cdb15731c12f1f22
cockers <cockerill.andrew@gmail.com>
2021-12-16 09:29:01 +0000

    changed url

--- jobs_lib/terratest_compute_engine_amd64.groovy
+++ jobs_lib/terratest_compute_engine_amd64.groovy
@@ -12,7 +12,7 @@ pipelineJob("terratest/terratest-compute-engine-amd64") {
 			scm {
 				git {
 					remote {
-						url("git@github.com:andycockers/jenkins-dsl.git")
+						url("https://github.com/andycockers/jenkins-dsl")
 						credentials("github")
 					}
 					branch("*/main")
