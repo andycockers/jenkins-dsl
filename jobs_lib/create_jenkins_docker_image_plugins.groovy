ffff219fcaa06cb481e1ee899bdabcb4a50323cc
cockers <cockerill.andrew@gmail.com>
2022-09-06 16:00:36 +0100

    build jenkins image w plugins

---
+++ jobs_lib/create_jenkins_docker_image_plugins.groovy
@@ -0,0 +1,23 @@
+pipelineJob("dsl-jobs/create-jenkins-docker-image-plugins") {
+	description()
+	keepDependencies(false)
+	properties {
+		nextBuildNumber(7)
+	}
+
+	definition {
+		cpsScm {
+			scm {
+				git {
+					remote {
+						url("git@github.com:andycockers/jenkins-dsl.git")
+						credentials("github")
+					}
+					branch("*/main")
+				}
+			}
+			scriptPath("jenkinsfile_lib/create_jenkins_docker_image_plugins")
+		}
+	}
+	disabled(false)
+}
